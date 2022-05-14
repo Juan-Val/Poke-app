58ebba0a32ebd2253b144b03631094bf369c9b75
Juan Valencia <juan.vo.11.2000@gmail.com>
2022-05-13 19:55:39 -0500

    Actu

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+
 export const Loading = () => {
+
+
   return (
     <div className='mx-auto w-14 h-14  animate-bounce m-14'>
-      <img src="../assets/img/pokebola.png" alt="Pokebola" />
+      <img src='https://cdn-icons-png.flaticon.com/512/914/914726.png' alt="Pokebola" />
     </div>
   )
 }
